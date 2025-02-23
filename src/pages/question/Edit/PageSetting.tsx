fd11e552aae0ecff3f56ce46d1809eb414eed21b
Fanceir <fx_official@outlook.com>
2025-02-23 16:02:09 +0800

    feat: 右侧页面设置栏完成

---
+++ src/pages/question/Edit/PageSetting.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: Fanceir fx_official@outlook.com
+ * @Date: 2025-02-23 15:40:12
+ * @LastEditors: Fanceir fx_official@outlook.com
+ * @LastEditTime: 2025-02-23 15:43:17
+ * @FilePath: /questionnaire/src/pages/question/Edit/PageSetting.tsx
+ * @Description: 主页面右侧的页面设置
+ */
+import { FC } from "react";
+const PageSetting: FC = () => {
+  return <div>页面设是置</div>;
+};
+export default PageSetting;
--- src/pages/question/Edit/RightPanel.tsx
+++ src/pages/question/Edit/RightPanel.tsx
@@ -1,11 +1,34 @@
+/*
+ * @Author: Fanceir fx_official@outlook.com
+ * @Date: 2025-02-19 14:12:09
+ * @LastEditors: Fanceir fx_official@outlook.com
+ * @LastEditTime: 2025-02-23 15:48:52
+ * @FilePath: /questionnaire/src/pages/question/Edit/RightPanel.tsx
+ * @Description:  右侧的页面属性页面设置
+ */
 import { Tabs } from "antd";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { FileTextOutlined, SettingOutlined } from "@ant-design/icons";
 import ComponentProp from "./ComponentProp";
+import PageSetting from "./PageSetting";
+import useGetComponentInfo from "@/Hooks/useGetComponentInfo";
+
+enum TabKeys {
+  PROP = "prop",
+  SETTING = "setting",
+}
+
 const RightPanel: FC = () => {
+  const [activeKey, setActiveKey] = useState(TabKeys.PROP);
+  const { selectedId } = useGetComponentInfo();
+  useEffect(() => {
+    if (selectedId) {
+      setActiveKey(TabKeys.PROP);
+    } else setActiveKey(TabKeys.SETTING);
+  }, [selectedId]);
   const tabsItems = [
     {
-      key: "prop",
+      key: TabKeys.PROP,
       label: (
         <span>
           <FileTextOutlined />
@@ -15,16 +38,20 @@ const RightPanel: FC = () => {
       children: <ComponentProp />,
     },
     {
-      key: "layers",
+      key: TabKeys.SETTING,
       label: (
         <span>
           <SettingOutlined />
           设置
         </span>
       ),
-      children: <div>页面设置</div>,
+      children: (
+        <div>
+          <PageSetting />
+        </div>
+      ),
     },
   ];
-  return <Tabs defaultActiveKey="prop" items={tabsItems} />;
+  return <Tabs activeKey={activeKey} items={tabsItems} />;
 };
 export default RightPanel;
